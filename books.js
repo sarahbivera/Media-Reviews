bb899d741a6921da5eb6e0542392b32f0e316afa
Sarah Bivera <163114668+gxcode-git@users.noreply.github.com>
2026-04-13 11:06:09 +0530

    external data file: books

---
+++ books.js
@@ -0,0 +1,4 @@
+const books = [
+  { id: 201, title: "Lord of the Rings", category: "Books", img: "https://via.placeholder.com/150", desc: "Wizard adventure.", author: "J.R.R. Tolkien" },
+  { id: 202, title: "The Hobbit", category: "Books", img: "https://via.placeholder.com/150", desc: "Fantasy journey.", author: "J.R.R. Tolkien" }
+];
