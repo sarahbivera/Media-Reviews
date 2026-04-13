9b14017459ec7694c19487853d4b994527d8f045
Sarah Bivera <163114668+sarahbivera@users.noreply.github.com>
2026-04-13 12:46:56 +0530

    add new entries

--- books.js
+++ books.js
@@ -1,4 +1,8 @@
 const books = [
-  { id: 201, title: "Lord of the Rings", category: "Books", img: "https://via.placeholder.com/150", desc: "Wizard adventure.", author: "J.R.R. Tolkien" },
-  { id: 202, title: "The Hobbit", category: "Books", img: "https://via.placeholder.com/150", desc: "Fantasy journey.", author: "J.R.R. Tolkien" }
+  { id: 201, title: "Harry Potter", category: "Books", img: "https://covers.openlibrary.org/b/id/7984916-L.jpg", desc: "Magic", author: "Rowling" },
+  { id: 202, title: "Hobbit", category: "Books", img: "https://covers.openlibrary.org/b/id/6979861-L.jpg", desc: "Fantasy", author: "Tolkien" },
+  { id: 203, title: "1984", category: "Books", img: "https://covers.openlibrary.org/b/id/7222246-L.jpg", desc: "Dystopia", author: "Orwell" },
+  { id: 204, title: "Atomic Habits", category: "Books", img: "https://covers.openlibrary.org/b/id/11153229-L.jpg", desc: "Self help", author: "Clear" },
+  { id: 205, title: "Alchemist", category: "Books", img: "https://covers.openlibrary.org/b/id/8279256-L.jpg", desc: "Philosophy", author: "Coelho" },
+  { id: 206, title: "Rich Dad", category: "Books", img: "https://covers.openlibrary.org/b/id/8091016-L.jpg", desc: "Finance", author: "Kiyosaki" }
 ];
